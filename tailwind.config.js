e0153b403c152b943a131b6403f21e4d34252103
Ferfix <fernandico06@gmail.com>
2025-07-20 20:45:44 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+module.exports = {
+  darkMode: 'class',
+  content: [
+    './src/**/*.astro',
+    './src/components/**/*.astro',
+    './src/layouts/**/*.astro',
+    './src/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: { extend: {} },
+  plugins: [],
+}
